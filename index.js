a8f3fc042e5731e208c52b959b6131aad93949cf
Hexiagon <seansmg@outlook.com>
2024-03-21 01:28:35 -0700

    Finished making the game playable, added sounds, added background, and added a time limit extension.

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var canvas=document.createElement("canvas");
 var ctx= canvas.getContext("2d");
-canvas.width=1000;
-canvas.height=1000;
+canvas.width=1005;
+canvas.height=1005;
 document.body.appendChild(canvas);
 var rows=8;
 var cols=3;
@@ -23,86 +23,125 @@ var up=false;
 var down=false;
 var counter=0;
 var soundEfx=document.getElementById("soundEfx");
-var soundGameOver="sounds/gameOver.wav";
-var soundCollected="sounds/collected.wav";
+var soundGameOver="sounds/mixkit-arcade-video-game-bonus-2044.wav";
+var soundSelectedSquare="sounds/mixkit-bonus-earned-in-video-game-2058.wav";
 var bgReady=false;
 var bgImage=new Image();
 bgImage.onload = function () {
     bgReady=true;
 };
 bgImage.src="images/background.jpg";
-var edgeReady=false;
-var edgeImage=new Image();
-edgeImage.onload =function(){
-    edgeReady=true;
-};
-edgeImage.src="images/edges.jpg";
-var edgeReady2=false;
-var edgeImage2=new Image();
-edgeImage2.onload =function(){
-    edgeReady2=true;
-};
-edgeImage2.src="images/edges2.jpg";
-var PlayerModelReady=false;
-var PlayerModelImage = new Image();
-PlayerModelImage.onload = function(){
-    PlayerModelReady=true;
-};
-PlayerModelImage.src="images/playerModel.png";
-var ItemReady=false;
-var ItemImage = new Image();
-ItemImage.onload = function(){
-    ItemReady=true;
-};
-ItemImage.src="images/item.png";
-var playermodel = {
-    speed: 256,
+var XPlayer = {
+    Shape: "X"
+}
+var OPlayer = {
+    Shape: "O"
+}
+var TopLeft={
+    Shape: "",
     x: 0,
     y: 0
 }
-var item = {
-    x: 0,
+var TopMiddle={
+    Shape: "",
+    x: 335,
     y: 0
 }
-let itemsCollected=0;
+var TopRight={
+    Shape: "",
+    x: 670,
+    y: 0
+}
+var CenterLeft={
+    Shape: "",
+    x: 0,
+    y: 335
+}
+var CenterMiddle={
+    Shape: "",
+    x: 335,
+    y: 335
+}
+var CenterRight={
+    Shape: "",
+    x: 670,
+    y: 335
+}
+var BottomLeft={
+    Shape: "",
+    x: 0,
+    y: 670
+}
+var BottomMiddle={
+    Shape: "",
+    x: 335,
+    y: 670
+}
+var BottomRight={
+    Shape: "",
+    x: 670,
+    y: 670
+}
+var AllSides=[];
 var keysDown = {};
+var mousePositionX;
+var mousePositionY;
+addEventListener("mousemove",function(e){
+    mousePositionX=e.clientX;
+    mousePositionY=e.clientY;
+});
 addEventListener("keydown",function(e){
-    keysDown[e.code]=true;
+    keysDown[e.keyCode]=true;
 }, false);
 addEventListener("keyup",function(e){
-    delete keysDown[e.code];
+    delete keysDown[e.keyCode];
 }, false);
-let countdown = 360;
+let countdown;
+let turnNumber;
+let Message="";
+var gameover=function(){
+    alert(Message);
+    soundEfx.src=soundGameOver;
+    soundEfx.play();
+    reset();
+}
 var update = function(modifier){
-    left = false;
-    right = false;
-    up=false;
-    down=false;
-    if(countdown>0){
-        countdown-=(1/30);
+    countdown-=1/60;
+    if(countdown<0){
+        Message="All players lose.";
+        gameover();
     }
-    else if(countdown<=0){
-        soundEfx.src=soundGameOver;
-        soundEfx.play();
-        alert("You lose.");
-        reset();
+    if(88 in keysDown&&turnNumber==0){
+        for(let i=0;i<AllSides.length;i++){
+            if(AllSides[i].Shape==""&&
+            mousePositionX<=(AllSides[i].x+335)
+            && mousePositionX>=(AllSides[i].x)
+            && mousePositionY<=(AllSides[i].y+335)
+            && mousePositionY>=(AllSides[i].y)){
+                AllSides[i].Shape=XPlayer.Shape;
+                turnNumber=1;
+                soundEfx.src=soundSelectedSquare;
+                soundEfx.play();
+                break;
+            }
+        }
     }
-    if(38 in keysDown && playermodel.y>(32+0)){
-        playermodel.y -= playermodel.speed*modifier;
-        up=true;
+    if(79 in keysDown&&turnNumber==1){
+        for(let i=0;i<AllSides.length;i++){
+            if(AllSides[i].Shape==""&&
+            mousePositionX<=(AllSides[i].x+335)
+            && mousePositionX>=(AllSides[i].x)
+            && mousePositionY<=(AllSides[i].y+335)
+            && mousePositionY>=(AllSides[i].y)){
+                AllSides[i].Shape=OPlayer.Shape;
+                turnNumber=0;
+                soundEfx.src=soundSelectedSquare;
+                soundEfx.play();
+                break;
+            }
+        }
     }
-    if(40 in keysDown && playermodel.y < canvas.height-(96+0)){
-        playermodel.y += playermodel.speed*modifier;
-        down=true;
-    }
-    if(37 in keysDown && playermodel.x>(32+0)){
-        playermodel.x -= playermodel.speed*modifier;
-        left=true;
-    }
-    if(39 in keysDown && playermodel.x<canvas.width-(96+0)){
-        playermodel.x += playermodel.speed*modifier;
-        right=true;
-    }
+    /*
     if(counter==5){
         curXFrame=++curXFrame%frameCount;
         counter=0;
@@ -127,47 +166,27 @@ var update = function(modifier){
         srcX=1*width;
         srcY=0*height;
     }
-    if(
-        playermodel.x<=(item.x+32)
-        && item.x<=(playermodel.x+32)
-        && playermodel.y<=(item.y+32)
-        && item.y<=(playermodel.y+32)
-    )
-    {
-        soundEfx.src=soundCollected;
-        soundEfx.play();
-        ++itemsCollected;
-        if(itemsCollected===4){
-            soundEfx.src= soundGameOver;
-            soundEfx.play();
-            alert("You won");
-            reset();
-        }
-    }
+    */
 }
 var render=function(){
     if(bgReady){
         ctx.drawImage(bgImage,0,0);
     }
-    if(edgeReady){
-        ctx.drawImage(edgeImage,0,0);
-        ctx.drawImage(edgeImage,0,968);
-    }
-    if(edgeReady2){
-        ctx.drawImage(edgeImage2,0,0);
-        ctx.drawImage(edgeImage2,968,0);
-    }
+    /*
     if(PlayerModelReady){
         ctx.drawImage(PlayerModelImage,srcX,srcY,width,height,playermodel.x,playermodel.y,width,height);
     }
-    if(ItemReady){
-        ctx.drawImage(ItemImage,item.x,item.y);
-    }
-    ctx.fillStyle="rgb(250,250,250)";
+    */
+    ctx.fillStyle="rgb(0,0,0)";
     ctx.font="24px Helvetica";
     ctx.textAlign="left";
     ctx.textBaseline="top";
-    ctx.fillText("Items collected: " + itemsCollected,32,32);
+    if(turnNumber===0){
+        ctx.fillText("X Player's turn (To place an X, press X). Timer: "+countdown,0,0);
+    }
+    else if(turnNumber===1){
+        ctx.fillText("O Player's turn (To place an O, press O). Timer: "+countdown,0,0);
+    }
 }
 var main=function(){
     var now=Date.now();
@@ -178,10 +197,20 @@ var main=function(){
     requestAnimationFrame(main);
 }
 var reset=function(){
+    /*
     playermodel.x=(canvas.width/2)-16;
     playermodel.y=(canvas.height/2)-16;
     item.x=32+(Math.random()*(canvas.width-96));
     item.y=32+(Math.random()*(canvas.height-96));
+    */
+    delete ctx;
+    countdown=360;
+    turnNumber=0;
+    for(let i=0;i<AllSides.length;i++){
+        AllSides[i].Shape="";
+    }
+    AllSides=[TopLeft,TopMiddle,TopRight,CenterLeft,CenterMiddle,
+    CenterRight,BottomLeft,BottomMiddle,BottomRight];
 }
 var then=Date.now();
 reset();
